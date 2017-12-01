1f43354569767fc02f476043917defd1a2e3b55d
Fredrik Wiklander <fredrik.wiklander@comhem.com>
2017-12-01 15:18:41 +0100

    Code to solve Day 1

---
+++ 2017/ConsoleApp1/ConsoleApp1/IDay.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp1
+{
+    public interface IDay
+    {
+        void Run();
+    }
+}
---
+++ 2017/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static string QuestionSeparator = "-------------------- {0} --------------------";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(QuestionSeparator, "Question 1");
+            new Day1().Run();
+            Console.WriteLine();
+            Console.WriteLine(QuestionSeparator, "Question 2");
+            ////new Day2().Run();
+            Console.Read();
+        }
+    }
+}
