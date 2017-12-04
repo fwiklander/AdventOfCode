7d011b73ed44c75074dd0679ea440f6deb8cc9cc
Fredrik Wiklander <fredrik.wiklander@comhem.com>
2017-12-04 14:52:35 +0100

    Added stopwatch to each day

--- 2017/DotNetCode/ConsoleRunner/Program.cs
+++ 2017/DotNetCode/ConsoleRunner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using AdventOfCode;
 
 namespace ConsoleApp1
@@ -10,19 +11,31 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
             Console.WriteLine(QuestionSeparator, "Day 1");
+            var watch = Stopwatch.StartNew();
             new Day1().Run();
+            watch.Stop();
+            Console.WriteLine($"Required time: {watch.ElapsedMilliseconds}ms");
             Console.WriteLine();
 
             Console.WriteLine(QuestionSeparator, "Day 2");
+            watch = Stopwatch.StartNew();
             new Day2().Run();
+            watch.Stop();
+            Console.WriteLine($"Required time: {watch.ElapsedMilliseconds}ms");
             Console.WriteLine();
 
             Console.WriteLine(QuestionSeparator, "Day 3");
+            watch = Stopwatch.StartNew();
             new Day3().Run();
+            watch.Stop();
+            Console.WriteLine($"Required time: {watch.ElapsedMilliseconds}ms");
             Console.WriteLine();
 
             Console.WriteLine(QuestionSeparator, "Day 4");
+            watch = Stopwatch.StartNew();
             new Day4().Run();
+            watch.Stop();
+            Console.WriteLine($"Required time: {watch.ElapsedMilliseconds}ms");
             Console.WriteLine();
 
             Console.WriteLine(QuestionSeparator, "DONE");
